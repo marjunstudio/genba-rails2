9ee85eb37a1bd8d559c06637ef5ea27eb4c042e0
marjunstudio <uebujie@gmail.com>
2023-04-21 17:10:00 +0900

    Add:Userモデルを作成しCRUDを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_071959) do
+ActiveRecord::Schema.define(version: 2023_04_21_073035) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", limit: 30, null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2023_04_21_071959) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
